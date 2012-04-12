6265daa756de4f29e33e255b10e4f543614f7b7a
Azelphur <azelphur@azelphur.com>
2012-04-12 05:06:22 +0100

    Removed always_trust Moved mpexes fingerprint into a variable

--- mpex.py
+++ mpex.py
@@ -10,15 +10,16 @@ class MPEx:
         self.gpg = gnupg.GPG()
         self.passphrase = passphrase
         self.mpex_url = 'http://polimedia.us/bitcoin/mpex.php'
+        self.mpex_fingerprint = 'F1B69921'
 
     def command(self, command):
         signed_data = self.gpg.sign(command, passphrase=self.passphrase)
-        encrypted_ascii_data = self.gpg.encrypt(str(signed_data), ['F1B69921'], passphrase=self.passphrase, always_trust=True)
+        encrypted_ascii_data = self.gpg.encrypt(str(signed_data), self.mpex_fingerprint, passphrase=self.passphrase)
         data = urllib.urlencode({'msg' : str(encrypted_ascii_data)})
         req = urllib2.Request(self.mpex_url, data)
         response = urllib2.urlopen(req)
         result = response.read()
-        return self.gpg.decrypt(result, passphrase=self.passphrase, always_trust=True)
+        return self.gpg.decrypt(result, passphrase=self.passphrase)
 
 if __name__ == '__main__':
     from getpass import getpass
