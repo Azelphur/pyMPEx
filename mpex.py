40db051f35cf87cc779322221cd21f7753e25458
Juraj Variny <rini17@gmail.com>
2013-01-16 19:23:54 +0100

    added file logging, enabled by default (since the output is likely to be needed later).

--- mpex.py
+++ mpex.py
@@ -6,6 +6,7 @@ import urllib2
 import sys
 from decimal import Decimal
 from argparse import ArgumentParser
+from datetime import datetime
 SATOSHI = Decimal('100000000.0')
 
 def parse_args():
@@ -13,28 +14,37 @@ def parse_args():
     parser.add_argument("command", help="MPEx command. It is necessary to quote it because of '|' special character, like: \"BUY|S.MPOE|1|100000\" ")
     parser.add_argument("-y","--noconfirm", help="disable confirmation prompt", action='store_true', required = False)
     parser.add_argument("-a","--use_agent", help="use GPG agent instead of asking for passphrase", action='store_true', required = False)
+    parser.add_argument("-l","--logfile", help="append command, encrypted and decrypted output to this logfile. By default mpex.log in current directory is used. Use -l - to disable file logging completely.", required = False)
     args = parser.parse_args()
     return args
 
 class MPEx:
-    def __init__(self, use_agent = False):
+    def __init__(self, use_agent = False, logfile=True):
         self.gpg = gnupg.GPG(use_agent=use_agent)
         self.mpex_url = 'http://mpex.co'
         self.mpex_fingerprint = 'F1B69921'
         self.passphrase = None
+        self.log = None
+        if(logfile):
+            if(logfile == True):
+                logfile = "mpex.log"
+            self.log = open(logfile,'a')
 
     def command(self, command):
+        if (self.log) :self.log.write(datetime.now().isoformat() + " " +command + "\n")
         signed_data = self.gpg.sign(command, passphrase=self.passphrase)
         encrypted_ascii_data = self.gpg.encrypt(str(signed_data), self.mpex_fingerprint, passphrase=self.passphrase)
         data = urllib.urlencode({'msg' : str(encrypted_ascii_data)})
         req = urllib2.Request(self.mpex_url, data)
         response = urllib2.urlopen(req)
         result = response.read()
+        if (self.log) :self.log.write(datetime.now().isoformat() + " " +result+ "\n")
         reply = str(self.gpg.decrypt(result, passphrase=self.passphrase))
         if not self.gpg.verify(reply):
             print '!!!WARNING!!!'
             print 'Invalid Signature, do not trust this data.'
         if reply == '': return None
+        if (self.log) : self.log.write(datetime.now().isoformat() + " " +reply+"\n")
         return reply
 
     def checkKey(self):
@@ -61,7 +71,14 @@ class MPEx:
 if __name__ == '__main__':
     from getpass import getpass
     args = parse_args()
-    mpex = MPEx(args.use_agent)
+    if args.logfile in ('-' ,''):
+        logfile = False  
+    elif args.logfile == None:
+        #not specified - use default log file
+        logfile = True
+    else:
+        logfile = args.logfile
+    mpex = MPEx(args.use_agent, logfile)
     if not mpex.checkKey():
         print 'You have not added MPExes keys. Please run...'
         print 'gpg --search-keys "F1B69921"'
